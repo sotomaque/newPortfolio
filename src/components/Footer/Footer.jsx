8fcf1a082c7758fdee23a679ee2799ac109e96f5
sotomaque <sotomaque@gmail.com>
2020-03-02 15:33:48 -0800

    renamed project in package.json

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -23,7 +23,7 @@ const Footer = () => {
               return (
                 <a
                   key={id}
-                  href={url || 'https://github.com/cobidev/gatsby-simplefolio'}
+                  href={url || 'https://github.com/sotomaque'}
                   rel="noopener noreferrer"
                   target="_blank"
                   aria-label={name}
@@ -35,9 +35,9 @@ const Footer = () => {
         </div>
         <hr />
         <p className="footer__text">
-          © {new Date().getFullYear()} - Template developed by{' '}
-          <a href="https://github.com/cobidev" target="_blank" rel="noopener noreferrer">
-            Jacobo Martínez
+          © {new Date().getFullYear()}
+          <a href="https://github.com/sotomaque" target="_blank" rel="noopener noreferrer">
+            Enrique Sotomayor
           </a>
         </p>
 
